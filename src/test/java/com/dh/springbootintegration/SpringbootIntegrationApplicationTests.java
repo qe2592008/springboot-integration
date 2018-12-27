08bfb69832bcc06cfab1748c7c5d75ef2f550be3
qe2592008 <qe259@163.com>
2018-12-27 16:33:32 +0800

    init

---
+++ src/test/java/com/dh/springbootintegration/SpringbootIntegrationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dh.springbootintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootIntegrationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
